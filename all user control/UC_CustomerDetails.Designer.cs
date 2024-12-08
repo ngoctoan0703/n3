75d535d9de9d3f8149a501ef3fc9218fe4cd811d
04-vu <tranminhvu1009@gmail.com>
2024-12-08 16:16:40 +0700

    vu

---
+++ all user control/UC_CustomerDetails.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace làm_việc_nhóm_3.all_user_control
+{
+    partial class UC_CustomerDetails
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.guna2ComboBox1 = new Guna.UI2.WinForms.Guna2ComboBox();
+            this.guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(36, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(526, 39);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Thông Tin Chi Tiết Khánh Hàng";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(576, 171);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Tìm Kiếm ";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(94, 233);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1763, 490);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // guna2ComboBox1
+            // 
+            this.guna2ComboBox1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2ComboBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.guna2ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.guna2ComboBox1.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2ComboBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2ComboBox1.Font = new System.Drawing.Font("Segoe UI", 10.8F);
+            this.guna2ComboBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
+            this.guna2ComboBox1.ItemHeight = 30;
+            this.guna2ComboBox1.Items.AddRange(new object[] {
+            "Chi Tiết Tất Cả Các Khách Hàng ",
+            "Khách Hàng Đang Ở ",
+            "Khách Hàng Đã Check Out "});
+            this.guna2ComboBox1.Location = new System.Drawing.Point(771, 160);
+            this.guna2ComboBox1.Name = "guna2ComboBox1";
+            this.guna2ComboBox1.Size = new System.Drawing.Size(290, 36);
+            this.guna2ComboBox1.Style = Guna.UI2.WinForms.Enums.TextBoxStyle.Material;
+            this.guna2ComboBox1.TabIndex = 6;
+            // 
+            // guna2Elipse1
+            // 
+            this.guna2Elipse1.BorderRadius = 30;
+            this.guna2Elipse1.TargetControl = this;
+            // 
+            // UC_CustomerDetails
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.guna2ComboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "UC_CustomerDetails";
+            this.Size = new System.Drawing.Size(1882, 852);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private Guna.UI2.WinForms.Guna2ComboBox guna2ComboBox1;
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
+    }
+}
---
+++ dashboard.cs
@@ -0,0 +1,69 @@
+﻿using làm_việc_nhóm_3.all_user_control;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace làm_việc_nhóm_3
+{
+    public partial class dashboard : Form
+    {
+        public dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void btnexit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnaddroom_Click(object sender, EventArgs e)
+        {
+            panelmoving.Left = btnaddroom.Left + 50;
+            // hiển thị control
+            uc_addroom1.Visible = true;
+            // ưu tiên thứ tự sẽ được đẩy lên trước 
+            uc_addroom1.BringToFront();
+        }
+
+        private void dashboard_Load(object sender, EventArgs e)
+        {
+            uc_addroom1.Visible = false;
+            uC_thanhtoan1.Visible = false;
+            uC_CustomerDetails1.Visible = false;
+            btnaddroom.PerformClick();
+        
+        }
+
+        private void btncr_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btncheckout_Click(object sender, EventArgs e)
+        {
+            panelmoving.Left = btncheckout.Left + 60;
+            uC_thanhtoan1.Visible = true;
+            uC_thanhtoan1.BringToFront() ;
+        }
+
+        private void uC_CustomerDetails1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCustomerDetial_Click(object sender, EventArgs e)
+        {
+            panelmoving.Left = btnCustomerDetial.Left + 60;
+            uC_CustomerDetails1.Visible = true;
+            uC_CustomerDetails1.BringToFront();
+
+        }
+    }
+}
